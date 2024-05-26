a93c8e2a71a69030cdb6e1b5dd50e9f3a97b81f7
larinskiy <s.larinskiy@yandex.ru>
2024-05-26 08:31:43 +0300

    Add colors

--- facefinder.py
+++ facefinder.py
@@ -8,6 +8,18 @@ from time import sleep
 import dns.resolver
 
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 def get_records(domain, type):
     out_records = set()
     try:
@@ -38,9 +50,10 @@ def get_ipv4_addresses(domains):
             print(f'Number of detected IP addresses: {
                 len(ipv4_addresses)}', file=file)
             print('IPs:\n', '\n'.join(ipv4_addresses), file=file)
-            print(f'[+] Discovered IPs collected in ips_{comp}.txt')
+            print(
+                f'[{bcolors.OKGREEN}+{bcolors.ENDC}] Discovered IPs collected in ips_{comp}.txt')
     else:
-        exit('[i] No discovered IPs')
+        exit(f'[{bcolors.OKBLUE}i{bcolors.ENDC}] No discovered IPs')
     return sorted(ipv4_addresses)
 
 
@@ -64,14 +77,14 @@ def check_domains_records(domains):
               len(not_found_addr)}', file=file)
         print('Domain names:\n', '\n'.join(not_found_addr), file=file)
         print(
-            f'[+] Results collected in domains_{comp}.txt')
+            f'[{bcolors.OKGREEN}+{bcolors.ENDC}] Results collected in domains_{comp}.txt')
     return found_addr
 
 
 def get_cidrs(ipv4_addresses):
     subnets = set()
     keywords = input(
-        "[?] Enter the keywords separated by a space to search for ASN (For example, Amazon AZ): ").lower().split()
+        f"[{bcolors.OKBLUE}?{bcolors.ENDC}] Enter the keywords separated by a space to search for ASN (For example, Amazon AZ): ").lower().split()
     bar = tqdm(ipv4_addresses, desc=f"Searching CIDRs", unit="ip")
     for addr in bar:
         try:
@@ -89,14 +102,15 @@ def get_cidrs(ipv4_addresses):
         with open(f'cidrs_{comp}.txt', 'w') as file:
             print(f'Number of subnets detected: {len(subnets)}', file=file)
             print('Subnets:\n', '\n'.join(subnets), file=file)
-        print(f'[+] Discovered CIDRs collected in cirds_{comp}.txt')
+        print(
+            f'[{bcolors.OKGREEN}+{bcolors.ENDC}] Discovered CIDRs collected in cirds_{comp}.txt')
     else:
-        print('[i] No discovered CIDRs')
+        print(f'[{bcolors.OKBLUE}i{bcolors.ENDC}] No discovered CIDRs')
     return subnets
 
 
 # Show logo, version, info
-print('\
+print(f'{bcolors.HEADER}\
 ███████╗ █████╗  ██████╗███████╗    ███████╗██╗███╗   ██╗██████╗ ███████╗██████╗ \n\
 ██╔════╝██╔══██╗██╔════╝██╔════╝    ██╔════╝██║████╗  ██║██╔══██╗██╔════╝██╔══██╗\n\
 █████╗  ███████║██║     █████╗      █████╗  ██║██╔██╗ ██║██║  ██║█████╗  ██████╔╝\n\
@@ -104,7 +118,7 @@ print('\
 ██║     ██║  ██║╚██████╗███████╗    ██║     ██║██║ ╚████║██████╔╝███████╗██║  ██║\n\
 ╚═╝     ╚═╝  ╚═╝ ╚═════╝╚══════╝    ╚═╝     ╚═╝╚═╝  ╚═══╝╚═════╝ ╚══════╝╚═╝  ╚═╝\n\
 A tool for searching domains, IPv4 and CIDR of companies by certificates\n\
-https://github.com/larinskiy/facefinder\n\n\
+https://github.com/larinskiy/facefinder{bcolors.ENDC}\n\n\
 Based on tools: crt.sh -> dns lookup -> whois\n\n\
 After program complete, check files out_domains.txt, out_ips.txt and out_cidrs.txt\n')
 
@@ -124,32 +138,34 @@ domain = set()
 if not args.domain_list:
     if not args.domain:
         comp = input(
-            "[?] Enter the company name or domain name to search for subdomains without spaces (For example, Amazon or amazon.com): ")
+            f"[{bcolors.OKBLUE}?{bcolors.ENDC}] Enter the company name or domain name to search for subdomains without spaces (For example, Amazon or amazon.com): ")
     else:
         comp = args.domain
-        print(f'[+] Company name/domain set to {comp}')
-    print('[i] Perfoming request to Crt.sh ...')
+        print(f'[{bcolors.OKGREEN}+{bcolors.ENDC}] Company name/domain set to {comp}')
+    print(f'[{bcolors.OKBLUE}i{bcolors.ENDC}] Perfoming request to Crt.sh ...')
     response = None
     while not response:
         try:
             response = pd.read_html(
                 f"https://crt.sh/?q={comp}&exclude=expired&group=none")
         except:
-            print('[-] Crt.sh is unavailable. Trying again after 10 seconds...')
+            print(f'[{bcolors.WARNING}!{
+                bcolors.ENDC}] Crt.sh is unavailable. Trying again after 10 seconds...')
             sleep(10)
-    print('[+] Got Crt.sh repsonse')
+    print(f'[{bcolors.OKGREEN}+{bcolors.ENDC}] Got Crt.sh repsonse')
     if response[1][1][0] != 'None found':
         identities = set()
         for record in set(response[2]["Matching Identities"]):
             identities = identities.union(record.split())
         domains = set(response[2]["Common Name"]).union(
             identities)
-        print(f'[+] {len(domains)} domain names successfully collected')
+        print(f'[{bcolors.OKGREEN}+{bcolors.ENDC}] {len(domains)
+                                                    } domain names successfully collected')
         domains = sorted(set([domain.strip() for domain in domains]))
     else:
-        exit('[-] No discovered certs on Cert.sh')
+        exit(f'[{bcolors.FAIL}-{bcolors.ENDC}] No discovered certs on Cert.sh')
 elif os.path.isfile(args.domain_list):
-    print('[+] Founded domain list file')
+    print(f'[{bcolors.OKGREEN}+{bcolors.ENDC}] Founded domain list file')
     with open(args.domain_list) as file:
         for record in file:
             domain = domain.union(record.strip())
@@ -157,36 +173,39 @@ elif os.path.isfile(args.domain_list):
 # Perfom MX and NS search
 if not args.primary_domain:
     prime_domain = input(
-        '[?] Do you want to perform MX/NS search? Enter primary domain name if you want, or enter to skip: [For.ex. amazon.com]')
+        f'[{bcolors.OKBLUE}?{bcolors.ENDC}] Do you want to perform MX/NS search? Enter primary domain name if you want, or enter to skip: [For.ex. amazon.com] ')
 else:
     prime_domain = args.primary_domain
-    print(f'[+] Primary domain name is set to {prime_domain}')
+    print(f'[{bcolors.OKGREEN}+{bcolors.ENDC}] Primary domain name is set to {prime_domain}')
 if prime_domain:
     mx_domains = get_records(prime_domain, 'MX')
     ns_domains = get_records(prime_domain, 'NS')
-    domains = set(mx_domains).union(set(ns_domains)).union(domains)
-    print(f'[i] {len(mx_domains)} MX names and {
-          len(ns_domains)} NS names was discovered')
+    if len(mx_domains) != 0 or len(ns_domains) != 0:
+        domains = set(mx_domains).union(set(ns_domains)).union(domains)
+        print(f'[{bcolors.OKGREEN}+{bcolors.ENDC}] {len(mx_domains)} MX names and {
+            len(ns_domains)} NS names was discovered')
+    else:
+        print(f'[{bcolors.OKBLUE}i{bcolors.ENDC}] MX and NS records now found')
 
 # Check domain records
 if len(domains):
     found_addr = check_domains_records(domains)
 else:
-    exit('[-] No discovered domains')
+    exit(f'[{bcolors.FAIL}-{bcolors.ENDC}] No discovered domains')
 
 # Check if each domain exists
 if len(found_addr) and not (args.ip_list):
     ipv4_addresses = get_ipv4_addresses(found_addr)
 elif os.path.isfile(args.ip_list):
-    print('[+] Founded IP list file')
+    print(f'[{bcolors.OKGREEN}+{bcolors.ENDC}] Founded IP list file')
     with open(args.ip_list) as file:
         for record in file:
             ipv4_addresses = ipv4_addresses.union(record.strip())
 else:
-    exit('[-] No discovered records for domains')
+    exit(f'[{bcolors.FAIL}-{bcolors.ENDC}] No discovered records for domains')
 
 # Get CIDRs for company
 if len(ipv4_addresses):
     get_cidrs(ipv4_addresses)
 else:
-    print('[-] No discovered IP addresses')
+    print(f'[{bcolors.FAIL}-{bcolors.ENDC}] No discovered IP addresses')
